24ff68af4122e3caf0cdc7373fab4fd5a0b28011
zyrgit <zzhaoyiran1@yeah.net>
2016-10-23 09:54:07 -0500

    modify client demo

--- demo/src/main/java/org/wsdmcup17/demo/DummyRevisionClassifier.java
+++ demo/src/main/java/org/wsdmcup17/demo/DummyRevisionClassifier.java
@@ -12,44 +12,42 @@ import org.slf4j.LoggerFactory;
 import org.wikidata.wdtk.dumpfiles.MwRevision;
 import org.wikidata.wdtk.dumpfiles.MwRevisionProcessor;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import ml.dmlc.xgboost4j.java.Booster;
+import ml.dmlc.xgboost4j.java.DMatrix;
+import ml.dmlc.xgboost4j.java.XGBoost;
+import ml.dmlc.xgboost4j.java.XGBoostError;
+
 /**
- * A Wikidata Toolkit-based revision processor that classifies all revisions
- * as non-vandalism and hence sends a score 0f 0.0 to the server for each
- * revision.
+ * A Wikidata Toolkit-based revision processor that classifies all revisions as
+ * non-vandalism and hence sends a score 0f 0.0 to the server for each revision.
  */
 public class DummyRevisionClassifier implements MwRevisionProcessor {
 
-	private static final Logger
-		LOG = LoggerFactory.getLogger(DummyRevisionClassifier.class);
-	
-	private static final String
-		LOG_MSG_STARTING = "Starting...",
-		LOG_MSG_CURRENT_STATUS = "Current status:",
-		LOG_MSG_FINAL_RESULT = "Final result:",
-		LOG_MSG_NUM_REVISIONS = "   Number of revisions: %s",
-		LOG_MSG_NUM_REVISIONS_REGISTERED = 
-			"   Number of registered revisions: %s";
+	private static final Logger LOG = LoggerFactory.getLogger(DummyRevisionClassifier.class);
 
-	private static final int
-		LOG_STATISTICS_INTERVAL = 10000;
-	
+	private static final String LOG_MSG_STARTING = "Starting...", LOG_MSG_CURRENT_STATUS = "Current status:",
+			LOG_MSG_FINAL_RESULT = "Final result:", LOG_MSG_NUM_REVISIONS = "   Number of revisions: %s",
+			LOG_MSG_NUM_REVISIONS_REGISTERED = "   Number of registered revisions: %s";
+
+	private static final int LOG_STATISTICS_INTERVAL = 10000;
+
 	private CSVPrinter resultPrinter;
 	private BlockingQueue<CSVRecord> metadataQueue;
 	private long lastLogTime;
 	private int numRevisions;
 	private int numRevisionsFromRegisteredUsers;
 
-	public DummyRevisionClassifier(
-		BlockingQueue<CSVRecord> metaQueue, CSVPrinter resultPrinter
-	) {
+	public DummyRevisionClassifier(BlockingQueue<CSVRecord> metaQueue, CSVPrinter resultPrinter) {
 		this.resultPrinter = resultPrinter;
 		this.metadataQueue = metaQueue;
 	}
 
 	@Override
-	public void startRevisionProcessing(
-		String siteName, String baseUrl, Map<Integer, String> namespaces
-	) {
+	public void startRevisionProcessing(String siteName, String baseUrl, Map<Integer, String> namespaces) {
 		LOG.info(LOG_MSG_STARTING);
 		lastLogTime = System.currentTimeMillis();
 	}
@@ -61,16 +59,16 @@ public class DummyRevisionClassifier implements MwRevisionProcessor {
 
 		// Classify revision
 		float classificationScore = classifyRevision(revision, metadata);
-		
+
 		// Send result so server.
 		sendClassificationResult(revision.getRevisionId(), classificationScore);
-		
+
 		// Update statistics for logging.
 		updateStatistics(revision, metadata);
 	}
-	
+
 	private float classifyRevision(MwRevision revision, CSVRecord metadata) {
-		// This is where an actual classification based on  the revision and
+		// This is where an actual classification based on the revision and
 		// its associated metadata should happen. Instead, we just assign a
 		// score of 0.0, effectively classifying the revision as non-vandalism.
 		System.out.println(revision.getContributor());
@@ -80,28 +78,34 @@ public class DummyRevisionClassifier implements MwRevisionProcessor {
 		System.out.println(revision.getTimeStamp());
 		System.out.println(revision.getModel());
 		System.out.println(metadata.toString());
+
+		try {
+			// reload model and data
+			Booster booster = XGBoost.loadModel("./model/xgb.model");
+			DMatrix testMat = new DMatrix("./model/dtest.buffer");
+			float[][] predicts2 = booster.predict(testMat);
+		} catch (Exception e) {
+			System.err.println("Caught Exception: " + e.getMessage());
+		}
+
 		return 0.0f;
 	}
 
-	private void sendClassificationResult(
-		long revisionId, float classificationScore
-	) {
+	private void sendClassificationResult(long revisionId, float classificationScore) {
 		try {
 			resultPrinter.print(revisionId);
 			resultPrinter.print(classificationScore);
 			resultPrinter.println();
 			resultPrinter.flush();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			LOG.error("", e);
 		}
 	}
-	
+
 	private CSVRecord getMetadata() {
 		try {
 			return metadataQueue.take();
-		}
-		catch (InterruptedException e) {
+		} catch (InterruptedException e) {
 			LOG.error("", e);
 			return null;
 		}
@@ -111,8 +115,7 @@ public class DummyRevisionClassifier implements MwRevisionProcessor {
 	public void finishRevisionProcessing() {
 		try {
 			resultPrinter.close();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			LOG.error("", e);
 		}
 		LOG.info(LOG_MSG_FINAL_RESULT);
@@ -131,10 +134,9 @@ public class DummyRevisionClassifier implements MwRevisionProcessor {
 			lastLogTime = currentTime;
 		}
 	}
-	
+
 	private void printStatistics() {
 		LOG.info(String.format(LOG_MSG_NUM_REVISIONS, numRevisions));
-		LOG.info(String.format(LOG_MSG_NUM_REVISIONS_REGISTERED,
-				numRevisionsFromRegisteredUsers));
+		LOG.info(String.format(LOG_MSG_NUM_REVISIONS_REGISTERED, numRevisionsFromRegisteredUsers));
 	}
 }

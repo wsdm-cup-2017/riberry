aa992e31e78376484d519468b233aad361214e09
zyrgit <zzhaoyiran1@yeah.net>
2016-11-16 14:29:44 -0600

    modified classifier

--- wsdm-classify/src/wsdm/Classifier.java
+++ wsdm-classify/src/wsdm/Classifier.java
@@ -1,14 +1,21 @@
 package wsdm;
 
+import java.awt.datatransfer.FlavorListener;
 import java.awt.print.Printable;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.lang.management.ThreadInfo;
+import java.lang.reflect.Array;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -21,43 +28,134 @@ import ml.dmlc.xgboost4j.java.XGBoostError;
 
 public class Classifier {
 	public static final float MISSING = -100.0f;
-	public static int NUMCOL = 119; // num of features, not include label
-
-	public static String BASEDIR_DATA = "/home/yiran/wsdm/featuretrain/";
-//	public static String BASEDIR_DATA = "../featuretrain/";
+	public static int NumFeature ;//= 119; // num of features, not include label
+	public static String My_IP;
 	
-	public static void readfloat(String filename, float[] data, float[] label){
-		Scanner scan;
-	    File file = new File(filename);
-        int i=0, pos=0, lpos=0;
-	    try {
-	        scan = new Scanner(file);
-	        while(scan.hasNextFloat())
-	        {
-	        	if (pos>=data.length || lpos>label.length) {
-	        		System.out.print("overflow "+scan.nextFloat());
-	        		System.out.print(" "+scan.nextFloat());
-	        		System.out.print(" "+scan.nextFloat());
-	        		System.out.print(" "+scan.nextFloat());
-	        		System.out.print(" "+scan.nextFloat());
-	        		System.out.print(" "+scan.nextFloat());
-	        		System.out.print(" "+scan.nextFloat());
-	        		break;
-				}
-	        	i=(i+1)%(1+NUMCOL);
-	        	if (i==1) {
-	        		label[lpos]=scan.nextFloat();
-	        		lpos++;
+	public static final String BASEDIR_DATA_home = "/home/yiran/wsdm/featuretrain/";
+	public static final String BASEDIR_DATA_lab = "./";
+	public static String BASEDIR_DATA ;
+	public static String[] flist = {"_2012_10","_2012_11","_2013_01","_2013_03","_2013_05","_2013_07","_2013_09","_2013_11","_2014_01","_2014_03","_2014_05","_2014_07","_2014_09","_2014_11","_2015_01","_2015_03","_2015_05","_2015_07","_2015_09"};
+	public static String DataFormat= "ExtactedFeatures%s_array.txt";
+	public static final String VandalizedFile = "vandalized_array.txt";
+	
+	/**
+	 * Aggregate array records from file, store in 1D float[] at offset.
+	 */
+	public static int aggregateDataAsFloat(String filename, float[] data, float[] label, int rowOffset){
+		String line;
+		int lpos = rowOffset;
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			while ((line = br.readLine()) != null) {
+				if ("".equals(line.trim())) { // consistent with getShape, otherwise vec len not match ! 
 					continue;
 				}
-	            data[pos]=scan.nextFloat(); pos++;
-	        }
-	    } catch (Exception e1) {
-	            e1.printStackTrace();
-	    }
-	    System.out.println("   stat: dp="+pos+", lp="+lpos);
+				String st[] = line.trim().split(" ");
+				if (st.length == NumFeature + 1) {
+					int k = 0;
+					for (k = 1; k <= NumFeature; k++) {
+						try {
+							float tmp = Float.parseFloat(st[k]);
+							if (tmp>-Float.MAX_VALUE && tmp<Float.MAX_VALUE) {
+								data[k - 1 + NumFeature * lpos] = tmp;
+							}else{
+								data[k - 1 + NumFeature * lpos] = MISSING;
+								//System.out.println("! Missing: "+tmp+" at row "+(1+lpos));
+							}
+						} catch (Exception e) {
+							data[k - 1 + NumFeature * lpos] = MISSING;
+						}
+					}
+					label[lpos] = Float.parseFloat(st[0]);
+				} else {
+					System.out.println(filename + ": invalid line at lpos=" + lpos+"\n\n"+line);
+					for (int k = 1; k <= NumFeature; k++) {
+						data[k - 1 + NumFeature * lpos] = data[k -1-NumFeature + NumFeature * lpos]; // copy last record 
+					}
+				}
+				lpos++;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    System.out.println("Move offset to "+lpos);
+	    return lpos;
 	}
 	
+	/**
+	 * Read array records from file, store in 1D float[], 1st elem in each record is label.
+	 */
+	public static void readDataAsFloat(String filename, float[] data, float[] label){
+		String line;
+		int lpos = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			while ((line = br.readLine()) != null) {
+				String st[] = line.trim().split(" ");
+				if (st.length == NumFeature + 1) {
+					int k = 0;
+					for (k = 1; k <= NumFeature; k++) {
+						try {
+							float tmp = Float.parseFloat(st[k]);
+							if (tmp>Float.MIN_VALUE && tmp<Float.MAX_VALUE) {
+								data[k - 1 + NumFeature * lpos] = tmp;
+							}else{
+								data[k - 1 + NumFeature * lpos] = MISSING;
+								System.out.println("! Missing: "+tmp+" at row "+(1+lpos));
+							}
+							
+						} catch (Exception e) {
+							data[k - 1 + NumFeature * lpos] = MISSING;
+						}
+					}
+					label[lpos++] = Float.parseFloat(st[0]);
+				} else {
+					System.out.println(filename + ": invalid col num at lpos=" + lpos);
+					break;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    System.out.println("readfloat() stat: lpos="+lpos);
+	}
+	
+	/**
+	 * aggregate [rows, cols] of data in files pos in flist[].
+	 */
+	public static int[] aggregateShape(int[] fn){
+		int rc[] = new int[2];
+		int n ;
+		int lines=0, col=0;
+		
+		for (n = 0; n < fn.length; n++) {
+			String filename = BASEDIR_DATA + String.format(DataFormat, flist[fn[n]]);
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(filename));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					if (!"".equals(line.trim())) {
+						lines++;
+						if (col == 0) {
+							col = line.split(" ").length;
+						}
+					}
+				}
+				reader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		rc[0]=lines;
+		rc[1]=col;
+		return rc;
+	}
+	
+	/**
+	 * return [rows, cols] of data in file.
+	 */
 	public static int[] getshape(String filename){
 		int rc[] = new int[2];
 		int lines=0, col=0;
@@ -80,24 +178,10 @@ public class Classifier {
 		rc[1]=col;
 		return rc;
 	}
-	
-	public static int countrows(String filename){
-		int lines = 0;
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(filename));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				if (!"".equals(line.trim())) {
-					lines++;
-				}
-			}
-			reader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return lines;
-	}
 
+	/**
+	 * check results of multi-Dim vector, vec[:]=pred[:]
+	 */
 	public static float checkPredicts(float[][] fPredicts, float[][] sPredicts) {
 		//System.out.println(fPredicts.length); // # rows
 		if (fPredicts.length != sPredicts.length) {
@@ -113,6 +197,10 @@ public class Classifier {
 		}
 		return ((float) right)/total;
 	}
+	
+	/**
+	 * check results of 1D prediction, truth[i]==pred[i][0]
+	 */
 	public static float getAccuracy(float[] truth, float[][] sPredicts) {
 		if (truth.length != sPredicts.length) {
 			return -1;
@@ -131,43 +219,69 @@ public class Classifier {
 	}
 
 	public static void trainClassifier() throws Exception {
-		// load file from text file, also binary buffer generated by xgboost4j
-//		DMatrix trainMat = new DMatrix(BASEDIR_DATA+"ExtactedFeatures_2012_11_svm.txt");
-//		DMatrix testMat = new DMatrix(BASEDIR_DATA+"ExtactedFeatures_2012_10_svm.txt");
-		String trainfile = BASEDIR_DATA+"ExtactedFeatures_2012_11_array.txt";
-		int trainsp[] = getshape(trainfile); // 427168
+		System.out.println("We have "+flist.length+" files + 1 vandalized");
+		int trainf [] = {0,1};
+		int testf [] =  {1,2};
+		
+		int trainsp[] = aggregateShape(trainf);
 		int trainrow=trainsp[0];
-		NUMCOL=trainsp[1]  -1   ;  // not label -1
-		String testfile = BASEDIR_DATA+"ExtactedFeatures_2012_10_array.txt";
-		int testsp[] = getshape(testfile); // 264594
+		NumFeature=trainsp[1]  -1   ;  // no label -1
+		
+		int testsp[] = aggregateShape(testf); 
 		int testrow = testsp[0];
-		System.out.println("stat: trainrow="+trainrow+", testrow="+testrow+", col="+NUMCOL);
 		
-		float[] traindata=new float[trainrow*NUMCOL];
-		float[] trainlabel=new float[trainrow];
+		String vanfile = BASEDIR_DATA+VandalizedFile;
+		int vansp[] = getshape(vanfile); 
+		int vanrow = vansp[0];
 		
-		float[] testdata=new float[testrow*NUMCOL];
+		int totalrow = trainrow+vanrow;
+		
+		System.out.println("Aggregate: train row="+totalrow+", test row="+testrow+", num feat="+NumFeature);
+		
+		float[] traindata=new float[totalrow*NumFeature];
+		float[] trainlabel=new float[totalrow];
+		
+		float[] testdata=new float[testrow*NumFeature];
 		float[] testlabel=new float[testrow];
 		
-		readfloat(trainfile,traindata,trainlabel);
-		readfloat(testfile, testdata, testlabel);
+		System.out.println("-----  train data  ------");
+		int offset = 0;
+		for (int i = 0; i < trainf.length; i++) {
+			String filename = BASEDIR_DATA + String.format(DataFormat, flist[trainf[i]]);
+			offset = aggregateDataAsFloat(filename, traindata, trainlabel, offset);
+			System.out.println(filename+" -> "+offset);
+		}
+		offset = aggregateDataAsFloat(vanfile, traindata, trainlabel, offset);
+		System.out.println(vanfile+" -> "+offset);
 		
+		System.out.println("-----  test data  ------");
+		offset=0;
+		for (int i = 0; i < testf.length; i++) {
+			String filename = BASEDIR_DATA + String.format(DataFormat, flist[testf[i]]);
+			offset = aggregateDataAsFloat(filename, testdata, testlabel, offset);
+			System.out.println(filename+" -> "+offset);
+		}
+		
+		System.out.print("1st rec:\n");
 		for(int i=0;i<traindata.length;i++){
-			if(i<NUMCOL){
+			if(i<NumFeature){
 				System.out.print(traindata[i]+" ");
 			}
 		}
 		System.out.println();
-		for(int i=0;i<trainlabel.length;i++){
-			if(i<2){
-				System.out.print(trainlabel[i]+",");
-			}
+		System.out.println("last rec:");
+		for(int i=traindata.length-NumFeature;i<traindata.length;i++){
+			System.out.print(traindata[i]+" ");
 		}
 		System.out.println();
+		System.out.print("first/last label:");
+		System.out.print(trainlabel[0]+",");
+		System.out.println(trainlabel[trainlabel.length-1]);
 		
-		DMatrix trainMat = new DMatrix(traindata, trainrow, NUMCOL, MISSING);
+		
+		DMatrix trainMat = new DMatrix(traindata, traindata.length/NumFeature, NumFeature, MISSING);
 		trainMat.setLabel(trainlabel);
-		DMatrix testMat = new DMatrix(testdata, testrow, NUMCOL, MISSING);
+		DMatrix testMat = new DMatrix(testdata, testdata.length/NumFeature, NumFeature, MISSING);
 		testMat.setLabel(testlabel);
 
 		HashMap<String, Object> params = new HashMap<String, Object>();
@@ -175,6 +289,7 @@ public class Classifier {
 		params.put("max_depth", 3);
 		params.put("silent", 1);
 		params.put("objective", "binary:logistic");
+		params.put("eval_metric","auc");
 
 		HashMap<String, DMatrix> watches = new HashMap<String, DMatrix>();
 		watches.put("train", trainMat);
@@ -209,11 +324,48 @@ public class Classifier {
 
 		// check the two predicts
 		System.out.println("Accuracy: "+getAccuracy(truth, predicts2));
-
-		
 	}
 
 	public static void main(String[] args) throws Exception {
+		try {
+	        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+	        while (interfaces.hasMoreElements()) {
+	            NetworkInterface iface = interfaces.nextElement();
+	            // filters out 127.0.0.1 and inactive interfaces
+	            if (iface.isLoopback() || !iface.isUp())
+	                continue;
+
+	            Enumeration<InetAddress> addresses = iface.getInetAddresses();
+	            while(addresses.hasMoreElements()) {
+	                InetAddress addr = addresses.nextElement();
+	                My_IP = addr.getHostAddress();
+	            }
+	        }
+	    } catch (SocketException e) {
+	        throw new RuntimeException(e);
+	    }
+		
+		System.out.println("IP: "+My_IP);
+		if (My_IP.startsWith("192.168")) {
+			BASEDIR_DATA = BASEDIR_DATA_home;
+		}else if (My_IP.startsWith("128.")) {
+			BASEDIR_DATA = BASEDIR_DATA_lab;
+		}else {
+			System.out.println("Wrong IP, exit...");
+			return;
+		}
+		
 		trainClassifier();
+	}
+	
+	
+	/////////////////////////////////  utils //////////////////////
+	public static float[] concatenate (float[] a, float[] b) {
+	    int aLen = a.length;
+	    int bLen = b.length;
+	    float[] c = (float[]) Array.newInstance(a.getClass().getComponentType(), aLen+bLen);
+	    System.arraycopy(a, 0, c, 0, aLen);
+	    System.arraycopy(b, 0, c, aLen, bLen);
+	    return c;
 	}
 }
